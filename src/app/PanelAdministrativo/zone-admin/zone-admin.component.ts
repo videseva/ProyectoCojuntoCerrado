fb3cb4f6c77eeecbdf42b3d710db1135b265b9ec
ldayanamercado <ldayanamercado@unicesar.edu.co>
2023-11-09 17:47:34 -0500

    alert

--- src/app/PanelAdministrativo/zone-admin/zone-admin.component.ts
+++ src/app/PanelAdministrativo/zone-admin/zone-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-zone-admin',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class ZoneAdminComponent {
 
+
+  deletezone(){
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Estás Seguro?",
+      text: "No Podrás Revertir Esto!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Si, Eliminar!",
+      cancelButtonText: "No, Cancelar!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire({
+          title: "Eliminado!",
+          text: "Su Archivo se ha Eliminado.",
+          icon: "success"
+        });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelado",
+          text: "Tu Archivo está a salvo :)",
+          icon: "error"
+        });
+      }
+    });
+  }
 }
