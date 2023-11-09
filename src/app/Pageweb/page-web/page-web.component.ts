4047709a0143f723a691a592c861a28a60cea397
ldayanamercado <ldayanamercado@unicesar.edu.co>
2023-11-08 22:58:47 -0500

    lamdingPage

--- src/app/Pageweb/page-web/page-web.component.ts
+++ src/app/Pageweb/page-web/page-web.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-web.component.css']
 })
 export class PageWebComponent {
+  scrollToSection(section: string) {
+    const element = document.getElementById(section);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+ 
 
+  ngOnInit() {
+    this.toggleDropdown();
+
+  }
+
+  // creando la clase para que el menu la despliegue 
+  toggleDropdown() {
+  const toggleBtn = document.querySelector('.toggle_btn') as HTMLElement;
+  const dropdownMenu = document.querySelector('.dropdown_menu') as HTMLElement;
+  const toggleBtnIcon = document.querySelector('.toggle_btn i') as HTMLElement;
+
+  toggleBtn.onclick = () => {
+    dropdownMenu.classList.toggle('open');
+    const isOpen = dropdownMenu.classList.contains('open');
+    toggleBtnIcon.classList.toggle('fa-xmark', isOpen);
+    toggleBtnIcon.classList.toggle('fa-bars', !isOpen);
+  };}
+
+ 
 }
