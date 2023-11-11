aadd3faebbccb011762412a458549ad143ce157b
JenniferCuello <jenniferacuello@unicesar.edu.co>
2023-11-10 22:51:18 -0500

    commict cambie de lugar el id de modelos

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,23 +1,23 @@
 export class usuario {
     
-    nombre: string;
     id: number;
+    nombre: string;
     genero: number;
     numero: number;
     correo: string;
     direccion: string;
 
     constructor(
-        nombre: string,
         id: number,
+        nombre: string,
         genero: number,
         numero: number,
         correo: string,
         direccion: string
     
     ) {
-        this.nombre = nombre || '';
         this.id = id || 0;
+        this.nombre = nombre || '';
         this.genero = genero || 0;
         this.numero = numero || 0;
         this.correo = correo || '';
