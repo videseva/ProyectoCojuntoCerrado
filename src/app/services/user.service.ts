94084e82e13cf6a915a85aa83075afeb9d9f4793
ldayanamercado <ldayanamercado@unicesar.edu.co>
2023-11-10 22:04:20 -0500

    services user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, of, tap, throwError } from 'rxjs';
+
+import { Usuario } from '../models/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private apiUrl = 'http://localhost:8000';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  post(user : Usuario): Observable<Usuario>{
+    return this.http.post<Usuario>(this.apiUrl+ '/users', user)
+    .pipe(
+      tap(_ => console.log('Usuario registrado')),
+      catchError(error =>{
+          console.log(error)
+          return of(error)
+      })
+    );
+  }
+
+  get(): Observable <Usuario[]>{
+    return this.http.get<Usuario[]>(this.apiUrl +'api/users').pipe(
+      tap(_ => console.log('Datos Encontrado')),
+      catchError(error =>{
+        console.log("error al buscar")
+        return of(error as Usuario[])
+      })
+      );
+  }
+
+  put(id: Number, usuario : Usuario):Observable <Usuario> {
+    id =usuario.id;
+    return this.http.put<Usuario>(this.apiUrl +'api/users/'+id,usuario).pipe(
+      tap(_ => console.log('Datos Encontrado')),
+      catchError(error =>{
+        console.log("error al buscar")
+        return of(error as Usuario)
+      })
+      );
+  }
+
+
 }
