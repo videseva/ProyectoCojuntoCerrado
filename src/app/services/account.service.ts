3037256a06f8f0017a66be5ce43f1ed1e45f4f8f
ldayanamercado <ldayanamercado@unicesar.edu.co>
2023-11-15 11:32:02 -0500

    up

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -11,7 +11,7 @@ export class AccountService {
   private apiUrl = 'http://localhost:4000/api/';
   constructor(private http: HttpClient) { }
 
-  post(cuenta : cuenta): Observable<cuenta>{
+  post(cuenta : any): Observable<cuenta>{
     return this.http.post<cuenta>(this.apiUrl+ 'store-account', cuenta)
     .pipe(
       tap(_ => console.log('cuenta registrado')),
@@ -43,26 +43,8 @@ export class AccountService {
     );
   }
 
-  put(id: Number, cuenta : cuenta):Observable <cuenta> {
-    id =cuenta.id;
-    return this.http.put<cuenta>(this.apiUrl +'edit-account/' +id ,cuenta).pipe(
-      tap(_ => console.log('Datos Encontrado')),
-      catchError(error =>{
-        console.log("error al buscar")
-        return of(error as cuenta)
-      })
-      );
-  }
 
-  delete(id: number, cuenta: cuenta): Observable<cuenta> {
-    return this.http.delete<cuenta>(this.apiUrl + 'delete-account/' + id).pipe(
-      tap(_ => console.log('Datos Eliminados')),
-      catchError(error => {
-        console.log("Error al eliminar");
-        return of(error as cuenta);
-      })
-    );
-  }
+
 
 
 
